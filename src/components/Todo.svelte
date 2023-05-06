88ce5540d171d5a6133eba0072b5be78c0ec68c4
mokmokahr <jbmok.workspace@gmail.com>
2023-05-06 22:12:21 +0900

    Initial commit

---
+++ src/components/Todo.svelte
@@ -0,0 +1,29 @@
+<script>
+    let id = 0;
+    $: totalTodos = tasks.length;
+    let tasks = '';
+    function handleSubmit(txt){
+        tasks = [...tasks, {id:++id, txt: txt, isCompleted: false}];
+        for(let i = 0; i<tasks.length; i++){
+            console.log(tasks[i]);
+        }
+    }
+    let taskValue = '';
+</script>
+
+<h1>TODO-LIST</h1>
+<div class="todoapp large">
+    <form class="add-task" on:submit|preventDefault={handleSubmit(taskValue)}>
+        <input type="text" bind:value = {taskValue} />
+        <button type="submit">추가하기</button>
+    </form>
+    <div class="todo-list">
+        <ul>
+            {#each tasks as task}
+            {#if task.isCompleted == false}
+            <li>[{task.id}] {task.txt}</li>
+            {/if}
+            {/each}
+        </ul>
+    </div>
+</div>
