84b5542d1af5759a9c5fab22c8f7d6b748e3cf37
mokmokahr <jbmok.workspace@gmail.com>
2023-05-06 23:12:24 +0900

    ADD: Add delte checkbox

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,14 +1,14 @@
 <script>
     let id = 0;
-    $: totalTodos = tasks.length;
     let tasks = '';
-    function handleSubmit(txt){
-        tasks = [...tasks, {id:++id, txt: txt, isCompleted: false}];
-        for(let i = 0; i<tasks.length; i++){
-            console.log(tasks[i]);
-        }
+    function handleSubmit(title){
+        tasks = [...tasks, {id:++id, title: title, isCompleted: false}];
     }
     let taskValue = '';
+    
+    //get date
+    const date = new Date();
+    console.log(Date());
 </script>
 
 <h1>TODO-LIST</h1>
@@ -17,13 +17,22 @@
         <input type="text" bind:value = {taskValue} />
         <button type="submit">추가하기</button>
     </form>
+    <div class="display-date">
+
+    </div>
     <div class="todo-list">
-        <ul>
-            {#each tasks as task}
-            {#if task.isCompleted == false}
-            <li>[{task.id}] {task.txt}</li>
-            {/if}
-            {/each}
-        </ul>
+        {#each tasks as task}
+        {#if task.isCompleted == false}
+        <div class="task-box uncompleted-block" id = "task{task.id}">
+            <input type="checkbox" bind:checked={task.isCompleted}>
+            <div class="task-title">{task.title}</div>
+        </div>
+        {:else}
+        <div class="task-box completed-block" id="task{task.id}">
+            <input type="checkbox" bind:checked={task.isCompleted}>
+            <div class="task-title"><strike>{task.title}</strike></div>
+        </div>
+        {/if}
+        {/each}
     </div>
 </div>
