4b1e8586e127d391bfe2b23b070fc6dced371a13
mokmokahr <jbmok.workspace@gmail.com>
2023-05-06 23:52:14 +0900

    ADD: Add date block

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,33 +1,48 @@
 <script>
     let id = 0;
     let tasks = '';
-    function handleSubmit(title){
-        tasks = [...tasks, {id:++id, title: title, isCompleted: false}];
+    const pushTask = (title)=>{
+        tasks = [...tasks, {id:++id, title: title, isCompleted: false, isShow:true}];
     }
     let taskValue = '';
     
     //get date
     const date = new Date();
-    console.log(Date());
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let day = date.getDay();
+    let stringDate = year+'.'+month+'.'+day;
+    
+    //handle user comment
+    let userComment = "화이팅!";
+
+    //delte comment
+    const deleteTask = (idx)=>{
+        tasks[idx].isShow = false;
+        tasks = tasks;
+    }
 </script>
 
-<h1>TODO-LIST</h1>
+<hr>
+<h1 class="display-date">
+    {stringDate}
+</h1>
+<hr>
+<h1>{userComment}</h1>
+<hr>
 <div class="todoapp large">
-    <form class="add-task" on:submit|preventDefault={handleSubmit(taskValue)}>
+    <form class="add-task" on:submit|preventDefault={pushTask(taskValue)}>
         <input type="text" bind:value = {taskValue} />
-        <button type="submit">추가하기</button>
+        <button type="submit">+</button>
     </form>
-    <div class="display-date">
-
-    </div>
     <div class="todo-list">
         {#each tasks as task}
-        {#if task.isCompleted == false}
+        {#if task.isCompleted == false && task.isShow == true}
         <div class="task-box uncompleted-block" id = "task{task.id}">
             <input type="checkbox" bind:checked={task.isCompleted}>
             <div class="task-title">{task.title}</div>
         </div>
-        {:else}
+        {:else if task.isShow == true}
         <div class="task-box completed-block" id="task{task.id}">
             <input type="checkbox" bind:checked={task.isCompleted}>
             <div class="task-title"><strike>{task.title}</strike></div>
