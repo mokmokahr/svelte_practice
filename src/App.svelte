832974a81b941d916fb1d5293931a7cb6f37f633
mokmokahr <jbmok.workspace@gmail.com>
2023-05-09 18:25:24 +0900

    DELETE: Delete files

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script>
 	import Root from "./components/Root.svelte"
-	import Title from "./components/Title.svelte";
 </script>
-<Title/>
+<Root/>
--- src/components/Title.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import { fade, blur, fly, slide, scale, draw } from 'svelte/transition';
-    let condition = false
-</script>
-<meta http-equiv="refresh" content="2; URL='./Root.svelte'">
-<p>tjdrhd</p>
