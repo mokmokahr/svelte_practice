5a2ea2f35e3454ce008c5d23929c202a01d91f93
mokmokahr <jbmok.workspace@gmail.com>
2023-05-09 09:04:41 +0900

    UPDATE: Update comment section

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,8 +1,17 @@
 <script>
+    import { comment } from "svelte/internal";
+
     let id = 0;
     let tasks = '';
     const pushTask = (title)=>{
-        tasks = [...tasks, {id:++id, title: title, isCompleted: false, isShow:true}];
+        if(taskValue !=""){
+            tasks = [...tasks, {id:++id, title: title, isCompleted: false, isShow:true}];
+        }else{
+            alert("할 일을 적어주세요!");       
+        }
+        if(taskValue != ""){
+            taskValue = "";
+        }
     }
     let taskValue = '';
     
@@ -10,19 +19,28 @@
     const date = new Date();
     let year = date.getFullYear();
     let month = date.getMonth()+1;
-    let day = date.getDay();
+    let day = date.getDate();
     let stringDate = year+'.'+month+'.'+day;
     
     //handle user comment
-    let userComment = "화이팅!";
-
+    let userInputComment = '';
+    let isPushed = false;
     //delte comment
     const delteCheckedBlock = ()=>{
         for(let i = 0; i<tasks.length; i++){
             if(tasks[i].isCompleted == true){
                 tasks[i].isShow = false;
-                //tasks = tasks;
             }
+        }
+    }
+    const deleteTask = (idx)=>{
+        console.log(idx);
+        tasks[idx-1].isShow = false;
+        tasks = tasks;
+    }
+    const cleanAllBlock = ()=>{
+        for(let i = 0; i<tasks.length; i++){
+            tasks[i].isShow = false;    
         }
     }
 </script>
@@ -32,27 +50,36 @@
     {stringDate}
 </h1>
 <hr>
-<h1 class="display-comment">{userComment}</h1>
+{#if isPushed == false}
+<form class="add-   comment" on:submit|preventDefault = {()=>{isPushed = true;}}>
+    <input type="text" placeholder="문구를 입력해주세요" bind:value={userInputComment}>
+    <button type="submit">입력</button>
+</form>
+{:else}
+<button on:click={()=>isPushed = false}>수정</button>
+{/if}
+<h1 class="display-comment">{userInputComment}</h1>
 <hr>
 <div class="display-tasks">
     <form class="add-task" on:submit|preventDefault={pushTask(taskValue)}>
-        <input type="text" bind:value = {taskValue} />
+        <input type="text" bind:value = {taskValue}/>
         <button type="submit">+</button>
     </form>
     <div class="todo-list">
         {#each tasks as task}
-        {#if task.isCompleted == false && task.isShow == true}
+        {#if task.isShow == true}
         <div class="task-box uncompleted-block" id = "task{task.id}">
+            <button on:click={deleteTask(task.id)}>-</button>
+            {#if task.isCompleted == false}
             <div class="task-title">{task.title}</div>
-            <input type="checkbox" bind:checked={task.isCompleted}>
-        </div>
-        {:else if task.isShow == true}
-        <div class="task-box completed-block" id="task{task.id}">
+            {:else if task.isCompleted == true}
             <div class="task-title"><strike>{task.title}</strike></div>
+            {/if}
             <input type="checkbox" bind:checked={task.isCompleted}>
         </div>
         {/if}
         {/each}
     </div>
-    <button on:click={()=>{delteCheckedBlock()}}>clean</button>
+    <button on:click={()=>{delteCheckedBlock()}}>완료한 일 삭제</button>
+    <button on:click={()=>{cleanAllBlock()}}>모두 지우기</button>
 </div>
